e33ecc0ab9b187671f1644d49a2fe282829f03e8
zeshan wajid <zeshan94.business@gmail.com>
2020-09-05 12:02:55 +0100

    update migration for product table

--- mystore/database/migrations/2020_09_05_105404_create_products_table.php
+++ mystore/database/migrations/2020_09_05_105404_create_products_table.php
@@ -15,6 +15,9 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('shopify_id');
+            $table->string('title');
+            $table->string('type');
             $table->timestamps();
         });
     }
