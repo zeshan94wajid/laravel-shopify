8329734e7c515f2c2e43e5b8f4585aa4987f2b46
zeshan wajid <zeshan94.business@gmail.com>
2020-09-05 10:32:59 +0100

    return all customers record as JSON response

--- mystore/app/Http/Controllers/CustomerController.php
+++ mystore/app/Http/Controllers/CustomerController.php
@@ -2,83 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * <p> Returns all customers for this app </p>
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
     public function index()
     {
-        //
+        $customers = Customer::all();
+        $message = count($customers) . ' found';
+
+        return $this->sendResponse($customers, $message);
     }
 
     /**
-     * Show the form for creating a new resource.
+     * <p> Updates the Customer tables with data from Shopify </p>
      *
-     * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function syncShopifyCustomers()
     {
         
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
